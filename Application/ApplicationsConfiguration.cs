cb4ec54d641c0b26fad5153250cad4d78480cddd
MNDG <61918019+mndg1@users.noreply.github.com>
2023-09-02 18:58:48 +0200

    Feature/applications module (#1)
    * Add Application module with DI
    
    * Add Unit Test
    
    * Format fix
    
    * Add CI
    
    * Update CI

---
+++ Application/ApplicationsConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Application;
+
+public class ApplicationsConfiguration
+{
+	public const string SECTION_NAME = "Applications";
+
+	public IDictionary<string, bool> StartupApplications { get; set; } = new Dictionary<string, bool>();
+}
---
+++ test/Applications/Application.Test/ApplicationFake.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Test;
+
+internal class ApplicationFake : IApplication
+{
+	public string Name => "Fake";
+	public bool HasStarted {  get; set; }
+
+	public Task StartAsync()
+	{
+		HasStarted = true;
+
+		return Task.CompletedTask;
+	}
+}
